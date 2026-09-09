cf2b91ce5de87602b9e4a43ea8ceef0de7e89db3
kho903 <gmldnr2222@naver.com>
2026-09-09 11:17:48 +0900

    chore: initialize Spring Security lab project

---
+++ src/main/java/com/jihun/securitylab/SpringSecurityLabApplication.java
@@ -0,0 +1,13 @@
+package com.jihun.securitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihun/securitylab/SpringSecurityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihun.securitylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
